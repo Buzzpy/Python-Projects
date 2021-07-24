0eb5749970597cd3b63ce52c20bba4ed6cbf89fd
SmallLion <78954171+SmallLion@users.noreply.github.com>
2021-07-24 11:01:36 +0530

    Grammer Correction
    Using correct grammar is very important in any language. This script can correct grammar in a sentence user have provided.

---
+++ grammer_correction.py
@@ -0,0 +1,6 @@
+from gingerit.gingerit import GingerIt
+text = input("Enter a sentence >>: ")
+corrected_text = GingerIt().parse(text)
+print(corrected_text['result'])
+
+# Coded with 💙 by Mr. Unity Buddy
